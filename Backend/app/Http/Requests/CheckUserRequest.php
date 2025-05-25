03ed4fb532dd5db25771d4ddd71b695c7602a31b
Nima <nimainjasta@gmail.com>
2025-05-25 14:09:48 +0330

    made controller for business sub categories

---
+++ Backend/app/Http/Requests/CheckUserRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CheckUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'mobile' => 'required|regex:/^09\d{9}$/',
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'mobile.required' => 'شماره موبایل الزامی است',
+            'mobile.regex' => 'فرمت شماره موبایل صحیح نیست',
+        ];
+    }
+}
