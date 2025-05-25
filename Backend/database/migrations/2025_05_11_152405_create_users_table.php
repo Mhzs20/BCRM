2a782c726f6a288f04e1ec55807c20b814f0a793
Nima <nimainjasta@gmail.com>
2025-05-25 13:54:08 +0330

    create user  table migrations

--- Backend/database/migrations/2025_05_11_152405_create_users_table.php
+++ Backend/database/migrations/2025_05_11_152405_create_users_table.php
@@ -4,24 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('phone_number' , 11)->unique();
-            $table->string('password_hash')->nullable();
-            $table->string('otp_secret');
-            $table->enum('role', ['Admin', 'SalonOwner', 'Staff']);
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
+            $table->string('name')->nullable();
+            $table->string('mobile', 11)->unique();
+            $table->string('password')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('otp_code', 4)->nullable();
+            $table->timestamp('otp_expires_at')->nullable();
+            $table->string('business_name')->nullable();
+            $table->unsignedBigInteger('business_category_id')->nullable();
+            $table->unsignedBigInteger('business_subcategory_id')->nullable();
+            $table->unsignedBigInteger('active_salon_id')->nullable();
             $table->timestamps();
+            
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
-}
+};
