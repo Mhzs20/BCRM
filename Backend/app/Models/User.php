78cfab002ab000e8c8d39d9e0da1c16f36ef7470
Nima <nimainjasta@gmail.com>
2025-05-25 14:38:55 +0330

    create user model

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -2,47 +2,92 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
+        'mobile',
         'password',
+        'business_name',
+        'business_category_id',
+        'business_subcategory_id',
+        'avatar',
+        'otp_code',
+        'otp_expires_at',
+        'active_salon_id',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
-        'remember_token',
+        'otp_code',
+        'otp_expires_at',
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'otp_expires_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
+    public function businessCategory()
+    {
+        return $this->belongsTo(BusinessCategory::class, 'business_category_id');
+    }
+
+    public function salons()
+    {
+        return $this->hasMany(Salon::class);
+    }
+
+    public function businessSubcategory()
+    {
+        return $this->belongsTo(BusinessSubcategory::class, 'business_subcategory_id');
+    }
+    public function activeSalon()
+    {
+        return $this->belongsTo(Salon::class, 'active_salon_id');
     }
 }
