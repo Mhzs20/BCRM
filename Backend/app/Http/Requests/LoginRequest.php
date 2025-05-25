d70f9c1e6e4f0d13916ad49fa49f0afd42e91e15
Nima <nimainjasta@gmail.com>
2025-05-25 14:34:15 +0330

    create login request

---
+++ Backend/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'mobile' => 'required|regex:/^09\d{9}$/',
+            'password' => 'required|string|min:8',
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'mobile.required' => 'شماره موبایل الزامی است',
+            'mobile.regex' => 'فرمت شماره موبایل صحیح نیست',
+            'password.required' => 'رمز عبور الزامی است',
+            'password.min' => 'رمز عبور باید حداقل ۸ کاراکتر باشد',
+        ];
+    }
+}
