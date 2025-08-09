eb12205aa34f3ce884d8bd66f6e5a35060a744dd
Nima <nimainjasta@gmail.com>
2025-08-09 15:37:37 +0330

    debuged api sms

---
+++ Backend/app/Http/Controllers/Api/AppController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\AppUpdate;
+use Illuminate\Http\Request;
+
+class AppController extends Controller
+{
+    public function latestVersion()
+    {
+        $latestUpdate = AppUpdate::latest()->first();
+
+        if ($latestUpdate) {
+            return response()->json($latestUpdate);
+        }
+
+        return response()->json(['message' => 'No update information found.'], 404);
+    }
+}
